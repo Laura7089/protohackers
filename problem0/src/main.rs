3df9ad80a51c875631ce11419bcaa0bf54bee5ed
Laura Demkowicz-Duffy <dev@demkowiczduffy.co.uk>
2024-03-04 18:28:36 +0000

    refactor(problem0): better logging

--- problem0/src/main.rs
+++ problem0/src/main.rs
@@ -3,12 +3,11 @@ use problem0::TcpEcho;
 use tokio::net::TcpListener;
 use tokio::io::{AsyncReadExt, AsyncWriteExt};
 use tower_service::Service;
-use tracing::debug;
+use tracing::{debug, debug_span, trace};
 
 const LISTEN_ADDR: &str = "0.0.0.0:5000";
 
 #[tokio::main]
-#[tracing::instrument]
 async fn main() -> std::io::Result<()> {
     tracing_subscriber::fmt::init();
     let listener = TcpListener::bind(LISTEN_ADDR).await?;
@@ -16,26 +15,29 @@ async fn main() -> std::io::Result<()> {
 
     loop {
         let (mut stream, addr) = listener.accept().await?;
-        debug!("new connection from {addr}");
-
         // TODO: this spawns a new task for every single incoming stream.
         // Can this cause resource starving?
         tokio::spawn(async move {
+            // TODO: don't run this string allocation unless we're in debug level
+            let addr = format!("{addr}");
+            let span = debug_span!("conn", addr);
+            debug!(parent: &span, "new connection");
+
             let mut service = TcpEcho;
             let mut buf = vec![0u8; 1024];
 
             loop {
                 let n = stream.read(&mut buf).await.unwrap();
-                debug!("received {n} bytes from {addr}");
+                trace!(parent: &span, "received {n} bytes");
                 if n == 0 {
                     // socket closed
-                    debug!("socket closed");
+                    debug!(parent: &span, "socket closed");
                     return;
                 }
 
                 let resp = service.call(buf[0..n].to_owned()).await.unwrap();
                 stream.write(&resp).await.unwrap();
-                debug!("send {} bytes to {addr}", resp.len());
+                trace!(parent: &span, "sent {} bytes", resp.len());
             }
         });
     }
